270b776e16639320d101bd5b71c5b460454d86c2
marisouzarodrigues0401-byte <marisouzarodrigues0401@gmail.com>
2025-11-28 09:31:13 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -28,3 +28,18 @@ def create_app():
             print("Banco inicializado com sucesso!")
 
     return app
+from flask_mail import Mail
+
+mail = Mail()
+
+def create_app():
+    app = Flask(__name__)
+    
+    # CONFIGURAÇÕES DO EMAIL
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 587
+    app.config["MAIL_USE_TLS"] = True
+    app.config["MAIL_USERNAME"] = "SEU_EMAIL@gmail.com"
+    app.config["MAIL_PASSWORD"] = "SENHA_DO_APP_GMAIL"
+
+    mail.init_app(app)
